20de8b85fad46f9f556d319d3e523e898d1549ff
acrowfliedover <leofanrong@gmail.com>
2023-07-31 23:41:40 -0700

    Check
    Check

---
+++ Assets/GameControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Refel's BPM=167
+    Queue<GameObject> Cells =new Queue<GameObject>();
+    Queue<GameObject> Notes =new Queue<GameObject>();
+
+
+    void Awake()
+    {
+        //load settings
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
